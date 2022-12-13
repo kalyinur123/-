35b60f4e7379cf04ca0ebfb0e980756f6ff0a82d
kalyinur123 <118823703+kalyinur123@users.noreply.github.com>
2022-12-13 10:20:43 +0600

    Add files via upload

---
+++ dz1_kalyinur-19-03/dz1/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Task from './Task'
+import Task2 from './Task2'
+
+
+function App() {
+  const name = []
+
+  
+
+  return (
+    <div className="App">
+        <Task/>
+        <Task2 user = {name2} />
+    </div>
+  )
+}
+
+export default App
---
+++ dz1_kalyinur-19-03/dz1/src/Task2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Task from './Task'
+
+function Task2(props) {
+
+  return (
+    <div>
+        
+        </div>
+  )
+}
+
+export default Task2
